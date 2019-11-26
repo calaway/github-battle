be9e1c1e3cff4308b79783134dbd46b7b3f99c92
Calaway <calaway@users.noreply.github.com>
2019-11-26 14:21:05 -0700

    Add players to Battle state

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -82,13 +82,49 @@ PlayerInput.propTypes = {
 }
 
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    }
+  }
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player
+    })
+  }
+
   render() {
+    const { playerOne, playerTwo } = this.state
+
     return (
       <React.Fragment>
         <Instructions />
-        <PlayerInput label='Player One' onSubmit={value => console.log('Value: ', value)} />
-        <PlayerInput label='Player Two' onSubmit={value => console.log('Value: ', value)} />
+
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            {playerOne === null && (
+              <PlayerInput
+                label='Player One'
+                onSubmit={player => this.handleSubmit('playerOne', player)}
+              />
+            )}
+            {playerTwo === null && (
+              <PlayerInput
+                label='Player Two'
+                onSubmit={player => this.handleSubmit('playerTwo', player)}
+              />
+            )}
+          </div>
+        </div>
       </React.Fragment>
     )
   }
+}
+
+Battle.propTypes = {
+  playerOne: PropTypes.string,
+  playerTwo: PropTypes.string
 }
