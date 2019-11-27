333fbc2a73ca5994a78fa8fa5cf451666e1d0d9f
Calaway <calaway@users.noreply.github.com>
2019-11-27 09:21:58 -0700

    Build battle results cards

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -137,7 +137,7 @@ export default class Popular extends React.Component {
           onUpdatedLanguage={this.updateLanguage} />
 
         {this.isLoading() && <p>LOADING</p>}
-        {error && <p>{error}</p>}
+        {error && <p className='center-text error'>{error}</p>}
         {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
       </>
     )
