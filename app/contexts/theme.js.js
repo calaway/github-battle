3561d9f20f9459b9ffd256584156df3847a763f1
Calaway <calaway@users.noreply.github.com>
2019-12-02 11:21:55 -0700

    Set up provider context containing theme data

---
+++ app/contexts/theme.js.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const {
+  Provider: ThemeProvider,
+  Consumer: ThemeConsumer
+} = React.createContext()
+
+export { ThemeProvider, ThemeConsumer }
+
--- app/index.js
+++ app/index.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import ThemeProvider from './contexts/theme.js'
 import './index.css'
 
 import Popular from './components/Popular'
 import Battle from './components/Battle'
 
 class App extends React.Component {
+  constructor() {
+    super(props)
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
+
   render() {
     return (
-      <div className='container'>
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className='container'>
+          <Battle />
+        </div>
+      </ThemeProvider>
     )
   }
 }
